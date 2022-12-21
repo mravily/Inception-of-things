ce2087335087e20fbb5103f052e4c46498fd1457
Med-Wo <mravily@student.42.fr>
2022-12-21 20:40:45 +0100

    Add ssh key in root :closed_lock_with_key:

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 --cluster-init"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=777 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 --cluster-init"
+
+# Copy the ssh keys on root for authorize the scp connection from worker
+sudo cp -f .ssh/* /root/.ssh/
 
 # Install mandatory dependencies for k3s and start the cluster
 curl -sfL https://get.k3s.io | sh -
