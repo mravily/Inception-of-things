1f06961eb86819def03542b40113c7b21585a054
Med-Wo <mravily@student.42.fr>
2022-12-24 02:00:38 +0100

    Config script for server :alembic:

---
+++ p2/scripts/server.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Add env var for setup the install of k3s
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1"
+
+# Copy the ssh keys on root for authorize the scp connection from worker
+sudo cp -f .ssh/* /root/.ssh/
+
+# Install mandatory dependencies for k3s and start the cluster
+curl -sfL https://get.k3s.io | sh -
+
+kubectl apply -f /vagrant/confs/.
