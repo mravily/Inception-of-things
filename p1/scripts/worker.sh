3fd2d5457ea616ee8fb0be2e9f047340436ac4fa
Med-Wo <mravily@student.42.fr>
2023-02-24 19:47:49 +0100

    lts: p1 v1.0 :bookmark:

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -4,11 +4,13 @@
 sudo mkdir /root/.ssh
 chmod 700 /root/.ssh
 
+cat .ssh/id_rsa.pub >> .ssh/authorized_keys
+
 # Copy the ssh keys for acces without password
 sudo cp -f .ssh/* /root/.ssh/
 
 # Get the node-token from the server for install k3s worker
-scp -o StrictHostKeyChecking=no root@192.168.56.110:/var/lib/rancher/k3s/server/node-token /tmp/node-token
+scp -o StrictHostKeyChecking=no root@$1:/var/lib/rancher/k3s/server/node-token /tmp/node-token
 
 # Add env var for setup the install of k3s
 export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file /tmp/node-token --node-ip=$2"
