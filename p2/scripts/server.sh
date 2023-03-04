cb08ae5985a22aaf02ca916282f92f6bb49f3681
Med-Wo <mravily@student.42.fr>
2023-03-05 00:48:55 +0100

    lts: p2 v1.1 :bookmark:

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
 # Add env var for setup the install of k3s
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1"
 
